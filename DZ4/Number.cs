845a11c21312d72602ccd7697ea9f3b0224e0f43
Maksim <mirror0791@mail.ru>
2024-09-18 17:45:59 +0300

    Initial commit

---
+++ DZ4/Number.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace DZ4
+{
+	public class Number
+	{
+
+		public void isNumber(int[] arr)
+		{
+            int number;
+            List<int> list = new List<int>(arr);
+            Console.Write("Введите число: ");
+            string word = Console.ReadLine();
+            if (int.TryParse(word, out int result))
+            {
+                number = result;
+                FindNumbers(number, list);
+            }
+            else
+            {
+                isNumber(arr);
+            }
+        }
+
+
+        public void FindNumbers(int num, List<int> list)
+        {
+            //{ 7, 5, 1, 3, 2, 5, 8, 2, 4, 5, 6, 9, 0, 12, 65, 7, 23 };
+            int count = 0;
+            while (list.Count>2)
+            {
+                int x = list[0];
+                list.RemoveAt(0);
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list.Contains(num-(x + list[i]))
+                        && list.IndexOf(list[i]) != list.IndexOf(num - (x + list[i])))
+                    {
+                        Console.WriteLine($"{num} = {x} + {list[i]} + {num - (x + list[i])}");
+                        count++;
+                    }
+                }
+
+            }
+            if(count ==0)
+            {
+                Console.WriteLine("Не найдено");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+
---
+++ DZ4/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DZ4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = new int[] { 7, 5, 1, 3, 2, 5, 8, 2, 4, 5, 6, 9, 0, 12, 65, 7, 23 };
+        Number number = new Number();
+        number.isNumber(arr);
+    }
+}
+
